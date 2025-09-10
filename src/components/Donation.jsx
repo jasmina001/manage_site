9c7dd0da2285bc89498e55b6002c721b7baeb51b
Jasmina <jorayevajasmina76@gmail.com>
2025-09-10 14:36:17 +0500

    add new donation file

--- src/components/Donation.jsx
+++ src/components/Donation.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import kartaImage from "../assets/karta.jpg"; // rasmni import qildik
-
+import kartaImage from "../assets/karta.jpg"; 
 const Donation = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-200 flex justify-center items-center p-6">
       <div className="bg-white shadow-2xl rounded-3xl p-8 max-w-lg w-full text-center transform hover:scale-[1.02] transition duration-300">
-        
-        {/* Kartochka rasmi */}
+
         <div className="mb-6">
           <img
             src={kartaImage}
@@ -14,8 +12,6 @@ const Donation = () => {
             className="mx-auto w-72 rounded-xl shadow-lg border border-gray-200"
           />
         </div>
-
-        {/* Matn */}
         <h2 className="text-2xl md:text-3xl font-extrabold text-gray-800 mb-4">
         Thank you for supporting our team!
         </h2>
